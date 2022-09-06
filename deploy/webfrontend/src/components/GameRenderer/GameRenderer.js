2a282e6e6b5fdd90d0ccd05ae745ee63c482184a
Alberto247 <albertocarboneri99@gmail.com>
2022-09-06 16:39:36 -0500

    broken scoreboard

--- deploy/webfrontend/src/components/GameRenderer/GameRenderer.js
+++ deploy/webfrontend/src/components/GameRenderer/GameRenderer.js
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import { useRef, useState, useEffect } from 'react';
 import { Row, Button, Collapse, Card, OverlayTrigger, Popover } from "react-bootstrap";
 import Form from 'react-bootstrap/Form'
+import Table from 'react-bootstrap/Table';
 
 import { HexGrid, Layout, Hexagon, Text, Pattern, Path, Hex, GridGenerator } from 'react-hexgrid';
 
@@ -26,111 +27,12 @@ const PLAYER_COLORS = {"-1": "white", null: "none", 1: "#008000", 2: "#0000FF", 
 const ID_map = { 1: "team1", 2: "team2", 3: "team3", 4: "team4", 5: "team5", 6: "team6", 7: "team7", 8: "team8", 9: "team9", 10: "team10", 11: "team11", 12: "team12", 13: "team13", 14: "team14", 15: "team15", 16: "team16" }
 let colors = { null: "none" }
 let tmp_ID_map = {null:"none"}
-let hexagon_map = {}
 let tick = 0;
+let crystal_tiles_seens={}
+let crystal_handled={}
+let deaths=[]
 let intervalid = undefined;
 
-// function edit_hexagon(hex) {
-
-//     const q = hex.q
-//     const r = hex.r
-//     const s = hex.s
-//     const point_type = hex.point_type
-//     const owner_ID = hex.owner_ID
-//     const current_value = hex.current_value
-
-//     let text = undefined
-//     let color = 'none'
-
-//     if (point_type === POINT_TYPES.FLAG) {
-//         text = `S ${current_value}`
-//         color = PLAYER_COLORS[owner_ID]
-//     } else if (point_type === POINT_TYPES.GRASS) {
-//         if (current_value !== 0) {
-//             text = `${current_value}`
-//         }
-//         color = PLAYER_COLORS[owner_ID]
-//     } else if (point_type === POINT_TYPES.WALL) {
-//         color = 'black'
-//     } else if (point_type === POINT_TYPES.FORT) {
-//         text = `H ${current_value}`
-//         color = PLAYER_COLORS[owner_ID]
-//     } else if (point_type === POINT_TYPES.CRYPTO_CRYSTAL) {
-//         text = `CC ${current_value}`
-//         color = 'green'
-//     } else if (point_type === POINT_TYPES.WEB_CRYSTAL) {
-//         text = `WC ${current_value}`
-//         color = 'yellow'
-//     } else if (point_type === POINT_TYPES.REV_CRYSTAL) {
-//         text = `RC ${current_value}`
-//         color = 'blue'
-//     } else if (point_type === POINT_TYPES.PWN_CRYSTAL) {
-//         text = `PC ${current_value}`
-//         color = 'purple'
-//     } else if (point_type === POINT_TYPES.MISC_CRYSTAL) {
-//         text = `MC ${current_value}`
-//         color = 'brown'
-//     } else if (point_type === POINT_TYPES.UNKNOWN_EMPTY) {
-//         color = 'grey'
-//     } else if (point_type === POINT_TYPES.UNKNOWN_OBJECT) {
-//         color = 'purple'
-//     }
-
-//     // const hide_hexagon = hide_hexagon_map[[hex.q, hex.r, hex.s]]
-//     if (player_pov !== undefined) {
-//         if (
-//             owner_ID !== player_pov &&
-//             !(
-//                 hex_map[[q + 1, r - 1, s]]?.owner_ID === player_pov ||
-//                 hex_map[[q - 1, r + 1, s]]?.owner_ID === player_pov ||
-//                 hex_map[[q + 1, r, s - 1]]?.owner_ID === player_pov ||
-//                 hex_map[[q - 1, r, s + 1]]?.owner_ID === player_pov ||
-//                 hex_map[[q, r + 1, s - 1]]?.owner_ID === player_pov ||
-//                 hex_map[[q, r - 1, s + 1]]?.owner_ID === player_pov
-//             )
-//         ) {
-//             // hide_hexagon.show()
-//             text = undefined
-//             if (point_type === POINT_TYPES.GRASS || point_type === POINT_TYPES.FLAG) {
-//                 color = 'grey'
-//             } else {
-//                 color = 'purple'
-//             }
-//         } else {
-//             // hide_hexagon.hide()
-//         }
-//     } else {
-//         // hide_hexagon.hide()
-//     }
-
-
-//     const hexagon = hexagon_map[[hex.q, hex.r, hex.s]]
-
-//     if (hexagon.fill() !== color) {
-//         hexagon.fill(color)
-//     }
-
-
-//     if (text) {
-//         const label = text_labels[[hex.q, hex.r, hex.s]]
-//         const { cx, cy } = hexagon.rbox()
-//         const p = DRAW.point(cx, cy)
-//         if (label) {
-//             label.show()
-//             if (label.text() !== text) {
-//                 label.text(text).center(p.x, p.y)
-//             }
-//         } else {
-//             let t = TEXT_GROUP.text(text).font({ size: 40 }).center(p.x, p.y)
-//             text_labels[[hex.q, hex.r, hex.s]] = t
-//         }
-//     } else {
-//         const label = text_labels[[hex.q, hex.r, hex.s]]
-//         if (label) {
-//             label.hide()
-//         }
-//     }
-// }
 
 function getTextColor(tile, pov, hex_map) {
     const [q, r, s]=tile["tuple"]
@@ -174,6 +76,54 @@ function getTextColor(tile, pov, hex_map) {
         color = 'purple'
     }
 
+    if([POINT_TYPES.CRYPTO_CRYSTAL, POINT_TYPES.WEB_CRYSTAL, POINT_TYPES.REV_CRYSTAL, POINT_TYPES.PWN_CRYSTAL, POINT_TYPES.MISC_CRYSTAL].includes(point_type) && owner_ID!=null){
+        if(!(owner_ID in crystal_handled)){
+            crystal_handled[owner_ID]=[point_type];
+            crystal_tiles_seens[owner_ID]=[];
+            crystal_tiles_seens[owner_ID].push([q+1, r-1, s].toString());
+            crystal_tiles_seens[owner_ID].push([q-1, r+1, s].toString());
+            crystal_tiles_seens[owner_ID].push([q+1, r, s-1].toString());
+            crystal_tiles_seens[owner_ID].push([q-1, r, s+1].toString());
+            crystal_tiles_seens[owner_ID].push([q, r+1, s-1].toString());
+            crystal_tiles_seens[owner_ID].push([q, r-1, s+1].toString());
+            crystal_tiles_seens[owner_ID].push([q+2, r, s-2].toString());
+            crystal_tiles_seens[owner_ID].push([q-2, r, s+2].toString());
+            crystal_tiles_seens[owner_ID].push([q+2, r-2, s].toString());
+            crystal_tiles_seens[owner_ID].push([q-2, r+2, s].toString());
+            crystal_tiles_seens[owner_ID].push([q+2, r-1, s-1].toString());
+            crystal_tiles_seens[owner_ID].push([q-2, r+1, s+1].toString());
+            crystal_tiles_seens[owner_ID].push([q, r+2, s-2].toString());
+            crystal_tiles_seens[owner_ID].push([q, r-2, s+2].toString());
+            crystal_tiles_seens[owner_ID].push([q-1, r+2, s-1].toString());
+            crystal_tiles_seens[owner_ID].push([q+1, r-2, s+1].toString());
+            crystal_tiles_seens[owner_ID].push([q-1, r-1, s+2].toString());
+            crystal_tiles_seens[owner_ID].push([q+1, r+1, s-2].toString());
+            crystal_tiles_seens[owner_ID].push([q, r, s].toString());
+        }
+        if(!(crystal_handled[owner_ID].includes(point_type))){
+            crystal_handled[owner_ID].push(point_type);
+            crystal_tiles_seens[owner_ID].push([q+1, r-1, s].toString());
+            crystal_tiles_seens[owner_ID].push([q-1, r+1, s].toString());
+            crystal_tiles_seens[owner_ID].push([q+1, r, s-1].toString());
+            crystal_tiles_seens[owner_ID].push([q-1, r, s+1].toString());
+            crystal_tiles_seens[owner_ID].push([q, r+1, s-1].toString());
+            crystal_tiles_seens[owner_ID].push([q, r-1, s+1].toString());
+            crystal_tiles_seens[owner_ID].push([q+2, r, s-2].toString());
+            crystal_tiles_seens[owner_ID].push([q-2, r, s+2].toString());
+            crystal_tiles_seens[owner_ID].push([q+2, r-2, s].toString());
+            crystal_tiles_seens[owner_ID].push([q-2, r+2, s].toString());
+            crystal_tiles_seens[owner_ID].push([q+2, r-1, s-1].toString());
+            crystal_tiles_seens[owner_ID].push([q-2, r+1, s+1].toString());
+            crystal_tiles_seens[owner_ID].push([q, r+2, s-2].toString());
+            crystal_tiles_seens[owner_ID].push([q, r-2, s+2].toString());
+            crystal_tiles_seens[owner_ID].push([q-1, r+2, s-1].toString());
+            crystal_tiles_seens[owner_ID].push([q+1, r-2, s+1].toString());
+            crystal_tiles_seens[owner_ID].push([q-1, r-1, s+2].toString());
+            crystal_tiles_seens[owner_ID].push([q+1, r+1, s-2].toString());
+            crystal_tiles_seens[owner_ID].push([q, r, s].toString());
+        }
+    }
+
     if (pov != -1) {
         if (
             owner_ID != pov &&
@@ -184,7 +134,7 @@ function getTextColor(tile, pov, hex_map) {
                 hex_map[[q - 1, r, s + 1].toString()]?.owner_ID == pov ||
                 hex_map[[q, r + 1, s - 1].toString()]?.owner_ID == pov ||
                 hex_map[[q, r - 1, s + 1].toString()]?.owner_ID == pov
-            )
+            ) && (!(parseInt(pov) in crystal_tiles_seens) || !(crystal_tiles_seens[parseInt(pov)].includes([q, r, s].toString())))
         ) {
             // hide_hexagon.show()
             text = ""
@@ -230,6 +180,9 @@ function HexagonalGrid(props) {
 function game_tick(edits, setMapStatus) {
     setMapStatus((oldStatus) => {
         for (const edit of edits) {
+            if(edit[1]==POINT_TYPES.FORT && oldStatus[edit[0]]["point_type"]==POINT_TYPES.FLAG){
+                deaths.push(oldStatus[edit[0]]["owner_ID"]);
+            }
             oldStatus[edit[0]]["point_type"] = edit[1]
             oldStatus[edit[0]]["owner_ID"] = edit[2]
             oldStatus[edit[0]]["current_value"] = edit[3]
@@ -238,22 +191,65 @@ function game_tick(edits, setMapStatus) {
     })
 }
 
+
+
 function LiveStats(props) {
     let players = {}
-    for (const hexagon of props.mapStatus) {
-        if (hexagon["owner_ID" != null]) {
+    function compare( a, b ) {
+        if ( a["ID"] in deaths ){
+          return -1;
+        }
+        if(b["ID"] in deaths){
+            return 1;
+        }
+        if ( a["value"]<b["value"] ){
+          return -1;
+        }else if(a["value"]>b["value"]){
+            return 1
+        }
+        if( a["land"]<b["land"]){
+            return -1
+        }else if(a["land"]>b["land"]){
+            return 1;
+        }
+        return 0;
+      }
+    for (const [key, hexagon] of Object.entries(props.mapStatus)) {
+        if (hexagon["owner_ID"] != null) {
             if (!(hexagon["owner_ID"] in players)) {
-                players[hexagon["owner_ID"]] = { "value": hexagon["current_value"], "land": 1 }
+                players[hexagon["owner_ID"]] = { "value": hexagon["current_value"], "land": 1, "ID":hexagon["owner_ID"] }
             } else {
                 players[hexagon["owner_ID"]]["value"] += hexagon["current_value"]
                 players[hexagon["owner_ID"]]["land"] += 1
             }
         }
     }
-    let ordered_scoreboard = []
+    let ordered_scoreboard = deaths.map((x)=>{return {"ID":x, "value":0, "land":0, "name":ID_map[tmp_ID_map[x]]}})
+    let tmp_arr=[]
     for (const [key, value] of Object.entries(players)) {
-
+        tmp_arr.push(value);
     }
+    tmp_arr.sort(compare);
+    for(const entry of tmp_arr){
+        ordered_scoreboard.push({"ID":entry["ID"], "value":entry["value"], "land":entry["land"], "name":ID_map[tmp_ID_map[entry["ID"]]]})
+    }
+    console.log(players)
+    ordered_scoreboard.reverse()
+    let rows=ordered_scoreboard.map((row, i)=><tr key={props.ID + row["ID"].toString()} style={{ backgroundColor: colors[row["ID"]] }}><td>{i+1}</td><td>{row["name"]}</td><td>{row["value"]}</td><td>{row["land"]}</td></tr>)
+
+    return <div style={{position:"absolute", float:"right", zIndex:"9", "top":100, "right":0}}><Table striped bordered hover>
+        <thead>
+            <tr style={{backgroundColor:"grey"}}>
+                <th>#</th>
+                <th>Name</th>
+                <th>Power</th>
+                <th>Land</th>
+            </tr>
+        </thead>
+        <tbody>
+            {rows}
+        </tbody>
+    </Table></div>
 }
 
 
@@ -317,7 +313,10 @@ function GameRenderer(props) {
         stop_ticking()
         tick = 0;
         setShownTick(tick)
+        crystal_handled=[];
+        crystal_tiles_seens=[];
         game_tick(props.gameHistory[0], setMapStatus);
+        
     }
 
     function btn_tick(event) {
@@ -338,7 +337,6 @@ function GameRenderer(props) {
         colors[entry["ID"]] = PLAYER_COLORS[entry["real_ID"]];
         tmp_ID_map[entry["ID"]]=entry["real_ID"];
     }
-    console.log(tmp_ID_map)
     let players = props.currentGameScoreboard.map((x) => x["ID"])
     players.sort()
 
@@ -351,13 +349,13 @@ function GameRenderer(props) {
         {options}
     </select>
 
-    const buttonRow = <div style={{ height: "5%", width: "100%", alignContent: "center" }}>Current Tick: {tick} <Button disabled={disabled} onClick={(e) => { btn_tick(e) }}>Tick</Button> <Button disabled={disabled} onClick={(e) => { start_ticking(speed) }}>Start</Button> <Button disabled={disabled} onClick={(e) => { stop_ticking(e) }}>Stop</Button> <Button disabled={disabled} onClick={(e) => { restart(e) }}>Restart</Button>{changePov}Speed:<Form.Range style={{ maxWidth: "20%", paddingTop: "15px" }} min={5} max={100} value={speed} onChange={(x) => { changeSlide(x) }} /></div>
+    const buttonRow = <div style={{ height: "5%", width: "100%", alignContent: "center" }}>Current Tick: {shownTick} <Button disabled={disabled} onClick={(e) => { btn_tick(e) }}>Tick</Button> <Button disabled={disabled} onClick={(e) => { start_ticking(speed) }}>Start</Button> <Button disabled={disabled} onClick={(e) => { stop_ticking(e) }}>Stop</Button> <Button disabled={disabled} onClick={(e) => { restart(e) }}>Restart</Button>{changePov}Speed:<Form.Range style={{ maxWidth: "20%", paddingTop: "15px" }} min={5} max={100} value={speed} onChange={(x) => { changeSlide(x) }} /></div>
 
 
 
 
 
-    return (<>{buttonRow}<HexagonalGrid pov={pov} mapStatus={mapStatus} /></>)
+    return (<>{buttonRow}<HexagonalGrid pov={pov} mapStatus={mapStatus} /><LiveStats mapStatus={mapStatus}/></>)
 }
 
 
