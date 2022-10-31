d08c1d62adc0b719cf79f17e5b969b64d9e2ca79
Alberto247 <albertocarboneri99@gmail.com>
2022-10-31 18:28:24 -0500

    dio

--- deploy/setup_all.py
+++ deploy/setup_all.py
@@ -23,7 +23,7 @@ time.sleep(3)
 client=docker.from_env()
 for x in range(1, 13):
     print(f"Setting up for team {x}")
-    print(client.images.build(path="../", dockerfile="Dockerfile.bot", tag=f"team{x}.registry.rising0.com:/bot/bot:latest"))
+    print(f"docker build ../ -f Dockerfile.bot -t team{x}.registry.rising0.com:/bot/bot:latest")
     os.system(f"docker login https://team{x}.registry.rising0.com: -u team{x} -p {passwords[x-1]}")
     os.system(f"docker push team{x}.registry.rising0.com:/bot/bot:latest")
 
