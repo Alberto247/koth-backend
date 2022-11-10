82212a965efc4213afb659efc4173f512bd72f9e
Alberto247 <albertocarboneri99@gmail.com>
2022-11-10 15:05:50 -0600

    fix

--- deploy/scoreboard-server/index.js
+++ deploy/scoreboard-server/index.js
@@ -14,7 +14,8 @@ app.get("/scoreboard", async (req, res)=>{
         let rawdata = fs.readFileSync("/results/current_scoreboard.json");
         res.status(200).json(JSON.parse(rawdata));
     } catch (err) {
-        res.status(500).end();
+        let rawdata = [{"teamId": 0, "score": 0}, {"teamId": 1, "score": 0}, {"teamId": 2, "score": 0}, {"teamId": 3, "score": 0}, {"teamId": 4, "score": 0}, {"teamId": 5, "score": 0}, {"teamId": 6, "score": 0}, {"teamId": 7, "score": 0}, {"teamId": 8, "score": 0}, {"teamId": 9, "score": 0}, {"teamId": 10, "score": 0}, {"teamId": 11, "score": 0}]
+        res.status(200).json(JSON.parse(rawdata));
     }
 })
 
@@ -23,7 +24,8 @@ app.get("/user-scoreboard", async (req, res)=>{
         let rawdata = fs.readFileSync("/results/user_scoreboard.json");
         res.status(200).json(JSON.parse(rawdata));
     } catch (err) {
-        res.status(500).end();
+        let rawdata = [{"teamId": 0, "score": 0}, {"teamId": 1, "score": 0}, {"teamId": 2, "score": 0}, {"teamId": 3, "score": 0}, {"teamId": 4, "score": 0}, {"teamId": 5, "score": 0}, {"teamId": 6, "score": 0}, {"teamId": 7, "score": 0}, {"teamId": 8, "score": 0}, {"teamId": 9, "score": 0}, {"teamId": 10, "score": 0}, {"teamId": 11, "score": 0}]
+        res.status(200).json(JSON.parse(rawdata));
     }
 })
 
