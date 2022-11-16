0e78af09e4fbf06d840544daee80d90792a875b4
Alberto247 <albertocarboneri99@gmail.com>
2022-11-16 15:22:21 -0600

    updated team names

--- deploy/webfrontend/src/config.js
+++ deploy/webfrontend/src/config.js
@@ -6,12 +6,12 @@ const teams = {1: "Kalmarunionen",
 4: "Water Paddler",
 5: "bootplug",
 6: "Tower of Hanoi",
-7: "gli sloveni",
+7: "Kokoska",
 8: "srdnlen",
 9: "MadrHacks",
 10: "NoPwnIntended",
 11: "HaverageBhackariEnjoyers",
-12: "team 12"
+12: "NOP team"
 }
 const SIDE = 15;
 
